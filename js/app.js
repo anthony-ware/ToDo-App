bb895b82599d684f779b9de13a2010adb042b623
anthonyware <anthony.ware10@yahoo.com>
2016-09-04 17:36:06 -0400

    change file structure

--- js/app.js
+++ js/app.js
@@ -1,34 +1,6 @@
 angular.module('todoApp',[])
 
-.controller('mainCtrl', function($scope, dataService){
-	 dataService.getTasks(function(response){
-			console.log(response.data);
-			$scope.tasks = response.data;	
-			});
-	 
-	 $scope.deleteTask = function(task, $index){
-	 	dataService.deleteTask(task);
-	 	$scope.tasks.splice($index, 1);
-	 };
 
-	 // $scope.saveTask = function(task){
-	 // 	dataService.saveTask(task)
-	 // };
-
-	})
-.service('dataService', function($http){
-		this.getTasks = function(callback){
-			$http.get('mock/tasks.json')
-		.then(callback)
-	};
-	this.deleteTask=function(task){
-
-	};
-
-	this.saveTask= function(task){
-
-	};
-});
 		
 
 
